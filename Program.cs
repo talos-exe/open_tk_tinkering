3695ac2ab6b312f8762c2e74423c6499d7071de2
talos-exe <bryantl1337@gmail.com>
2026-02-19 22:06:29 -0600

    Create a window

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using OpenTK_Tinkering;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (Window window = new Window(800, 600, "OpenTK_Window"))
+        {
+            window.Run();
+        }
+    }
+}
---
+++ Window.cs
@@ -0,0 +1,29 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace OpenTK_Tinkering
+{
+    public class Window : GameWindow
+    {
+        /// <summary>
+        /// Creates a constructor of the standard OpenTK GameWindow.
+        /// </summary>
+        /// <param name="width">The width of the window, in pixels.</param>
+        /// <param name="height">The height of the window, in pixels.</param>
+        /// <param name="title">The title of the window.</param>
+        public Window(Int32 width, Int32 height, String title) : base(GameWindowSettings.Default, new NativeWindowSettings() { ClientSize = (width, height), Title = title }) { }
+
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            base.OnUpdateFrame(args);
+
+            if (KeyboardState.IsKeyDown(Keys.Escape))
+            {
+                Close();
+            }
+        }
+
+    }
+}
